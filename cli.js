5a9f0d7233305817f496398da6200bf62cc13cef
Damian Bushong <katana@codebite.net>
2014-10-16 04:18:13 -0500

    hammering out the structure for cli.js some more

--- cli.js
+++ cli.js
@@ -20,18 +20,35 @@ cli.version(pkg.version)
 
 cli.command('populate')
 	.description('populate the database with a large set of API queries')
+	.option('--max-depth <depth>', 'maximum depth for API queries to reach (0 < depth <= 1000), default 1000')
+	.action(function() {
+		// todo
+	})
 
 cli.command('scan')
 	.description('run TrainerRed in weekly scan mode, creating a report via moderator mail')
+	.option('-d. --days <days>', 'number of days of material to use in the report, default 7')
+	.option('--no-mail', 'do not send modmail report')
+	.action(function() {
+		// todo
+	})
 
-// make this a --no-mail flag for the above instead?
-cli.command('silentscan')
-	.description('run TrainerRed in weekly scan mode, without creating a report')
+cli.command('domain <domain>')
+	.description('run TrainerRed against a supplied domain')
+	.action(function() {
+		// todo
+	})
 
-cli.command('domain')
-	.description('run TrainerRed against ')
+cli.command('user <user>')
+	.description('run TrainerRed against a supplied user')
+	.action(function() {
+		// todo
+	})
 
-cli.command('user')
-	.description('')
+cli.command('setup')
+	.description('setup prompts for required config values, builds config file')
+	.action(function() {
+		// todo
+	})
 
 cli.parse(process.argv)
