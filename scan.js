4f55721d7da6b75ffc53c9aa23a3b23f73f93fb9
Damian Bushong <katana@odios.us>
2014-10-19 12:12:42 -0500

    fix removal rate in scan.js

--- scan.js
+++ scan.js
@@ -86,7 +86,7 @@ trainerred.auth()
 		var total = res[0], removed = res[1], users = res[2], domains = res[3]
 		console.log('done grabbing db entries, sending modmail')
 
-		var removalRate = (removed == 0) ? 0 : Math.round(removed / total, -3) * 100
+		var removalRate = (removed == 0) ? 0 : Math.round((removed / total) * 1000) / 10
 
 		var msg = '## TrainerRed initial database population complete.'
 		msg += '\n\nTrainerRed has identified ' + total + ' entries (' + removed + ' removals; ' + removalRate + '% removal rate) within the last 7 days for analysis.'
