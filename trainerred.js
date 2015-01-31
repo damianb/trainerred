53e3718c2f87fafc9dbbd1ce8bc1610a856237e0
Damian Bushong <katana@odios.us>
2015-01-31 10:59:57 -0600

    update copyright year

--- trainerred.js
+++ trainerred.js
@@ -2,7 +2,7 @@
 //
 // TrainerRed - subreddit submission analysis bot
 // ---
-// @copyright (c) 2014 Damian Bushong <katana@odios.us>
+// @copyright (c) 2014-2015 Damian Bushong <katana@odios.us>
 // @license MIT license
 // @url <https://github.com/damianb/>
 // @reddit <https://reddit.com/u/katana__>
